e3c5afd5b528a9545f5bafd4e165c0852d3c64dd
VictorMojica22 <72884892+VictorMojica22@users.noreply.github.com>
2020-11-20 10:51:48 -0600

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$("h1 span, h2 span, h3 span").text("ríos");
+	$('#rioUno').replaceWith('<img src="riouno.jpg">')
+	$('#rioDos').replaceWith('<img src="riodos.jpg">')
+})
+$("h2").css('background-color',"#00FF00");
+$("titRio").css('color',"#00BFFF")
